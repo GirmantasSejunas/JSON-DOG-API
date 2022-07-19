c74ebf29482823af5cd2d3c73110dc27289799ed
Girmantas <girmantas123@gmail.com>
2022-07-19 14:43:36 +0300

    end with fetch

--- posts.js
+++ posts.js
@@ -1,8 +1,3 @@
-// let queryParams = document.location.search
-// let urlParams = new URLSearchParams(queryParams)
-// let postUserId = urlParams.get('post_userId')
-// console.log(postUserId)
-// let postWrapper = document.querySelector('#postWrapper')
 
 import headerFnc from './header.js'
 import {firstLetterToUppercase} from './function.js'
@@ -11,7 +6,7 @@ import {firstLetterToUppercase} from './function.js'
 let queryParams = document.location.search;
 let urlParams = new URLSearchParams(queryParams);
 let userId = urlParams.get('post_userId');
-let postUserId = urlParams.get('user_id')
+// let postUserId = urlParams.get('user_id')
 
 let postsWrapper = document.querySelector('#posts-wrapper');
 let postsListTitle = document.createElement('h2');
@@ -31,20 +26,16 @@ function init(){
 
 }
 function renderPostsByUserId(){
-  fetch(`https://jsonplaceholder.typicode.com/users/${userId}/posts`)
+  fetch(`https://jsonplaceholder.typicode.com/users/${userId}/posts?_expand=user`)
   .then(res => res.json())
   .then(posts => {
-    fetch('https://jsonplaceholder.typicode.com/users/' + userId)
-      .then(res => res.json())
-      .then(user => {
-      
-        postsListTitle.textContent = `Posts of ${user.name}:`;
-        posts.map(post => {
-          let postItem = document.createElement('li');
-          postItem.innerHTML = `<a href="./post.html?post_id=${post.id}">${firstLetterToUppercase(post.title)}</a>`;
-          postsList.prepend(postItem);
+  
+    posts.map(post => {
+      postsListTitle.textContent = `Posts of ${post.user.name}:`;
+      let postItem = document.createElement('li');
+      postItem.innerHTML = `<a href="./post.html?post_id=${post.id}">${firstLetterToUppercase(post.title)}</a>`;
+      postsList.prepend(postItem);
        
-        })
       })
   })
 }
